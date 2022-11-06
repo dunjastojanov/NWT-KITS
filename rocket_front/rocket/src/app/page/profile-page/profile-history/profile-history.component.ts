6e7435f8784f4ca9b914387d219c2de089c9bc98
Dunja <dunjastojanov2@gmail.com>
2022-11-06 17:25:55 +0100

    #32 Created profile page, profile history component and detailed route modal

---
+++ rocket_front/rocket/src/app/page/profile-page/profile-history/profile-history.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-history',
+  templateUrl: './profile-history.component.html',
+  styleUrls: ['./profile-history.component.css']
+})
+export class ProfileHistoryComponent implements OnInit {
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+
+
+
+}
