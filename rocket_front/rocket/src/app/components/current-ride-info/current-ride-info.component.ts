d2aa00ad32cac5bdf57864fe752913695abcdeca
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2022-11-12 21:33:14 +0100

    Make UI changes

--- rocket_front/rocket/src/app/components/current-ride-info/current-ride-info.component.ts
+++ rocket_front/rocket/src/app/components/current-ride-info/current-ride-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CurrentRide } from 'src/app/interfaces/Ride';
 
 @Component({
@@ -7,36 +7,19 @@ import { CurrentRide } from 'src/app/interfaces/Ride';
   styleUrls: ['./current-ride-info.component.css']
 })
 export class CurrentRideInfoComponent implements OnInit {
-
-  currentRide : CurrentRide | null;
+  @Input('ride') currentRide! : CurrentRide;
+  
   openReportModal = false;
 
   constructor() { 
-    this.currentRide = null;
   }
 
   ngOnInit(): void {
-    this.getCurrentRide();
-  }
-
-  getCurrentRide(): void {
-    //this.currentRide = this.rideService.getCurrentRide();
-    this.currentRide = {
-      rideId: "1", 
-      ridingPals: [ {id: "1", firstName: "One", lastName:"Pal", email:"palone@gmail.com"}, {id: "1", firstName: "Tu", lastName:"Pal", email:"paltwo@gmail.com"} ],
-      isSplitFair: true,
-      driver: {id: "1", firstName: "Best", lastName:"Driver", profilePicture:"profpic"},
-      startAddress: "Adresa broj 1",
-      endAddress: "Adresa broj 2",
-      price: 1150,
-      estimatedTime: "1h 15min",
-      vehicleLocation: {longitude: 40.151515615, latitude: 42.156516515},
-      minutesToCome: 7,
-      isRouteFavorite: false
-    }
+    
   }
 
   toggleReportModal() {
+    console.log(this.openReportModal)
     this.openReportModal = !this.openReportModal;
   }
 }
--- rocket_front/rocket/src/app/page/current-ride-page/current-ride-page.component.ts
+++ rocket_front/rocket/src/app/page/current-ride-page/current-ride-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentRide } from 'src/app/interfaces/Ride';
 
 @Component({
   selector: 'current-ride-page',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-ride-page.component.css']
 })
 export class CurrentRidePageComponent implements OnInit {
+  currentRide : CurrentRide | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.currentRide = null;
   }
 
+  ngOnInit(): void {
+    this.getCurrentRide();
+  }
+
+  getCurrentRide(): void {
+    //this.currentRide = this.rideService.getCurrentRide();
+    this.currentRide = {
+      rideId: "1", 
+      ridingPals: [ {id: "1", firstName: "One", lastName:"Pal", email:"palone@gmail.com"}, {id: "1", firstName: "Tu", lastName:"Pal", email:"paltwo@gmail.com"} ],
+      isSplitFair: true,
+      driver: {id: "1", firstName: "Best", lastName:"Driver", profilePicture:"profpic"},
+      startAddress: "Adresa broj 1",
+      endAddress: "Adresa broj 2",
+      price: 1150,
+      estimatedTime: "1h 15min",
+      vehicleLocation: {longitude: 40.151515615, latitude: 42.156516515},
+      minutesToCome: 7,
+      isRouteFavorite: false
+    }
+  }
 }
