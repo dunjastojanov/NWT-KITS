a6dd75440040299232336cc3aa2523064ddbfc43
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2023-01-02 22:04:59 +0100

    Add choosing alternative routes

--- rocket_front/rocket/src/app/shared/utils/map/map/map.component.ts
+++ rocket_front/rocket/src/app/shared/utils/map/map/map.component.ts
@@ -1,129 +1,280 @@
 import { AfterViewInit, Component, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import axios from 'axios';
-import * as L from 'leaflet'
-import * as lrm from 'leaflet-routing-machine'
+import * as L from 'leaflet';
+import * as lrm from 'leaflet-routing-machine';
 import { RouteService } from 'src/app/components/routes/route.service';
 import { Destination } from 'src/app/interfaces/Destination';
-import { DestinationsAction, DestinationsActionType } from 'src/app/shared/store/destinations-slice/destinations.actions';
+import {
+  DestinationsAction,
+  DestinationsActionType,
+} from 'src/app/shared/store/destinations-slice/destinations.actions';
 import { StoreType } from 'src/app/shared/store/types';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements AfterViewInit {
-  @Input('height') height!: string
+  @Input('height') height!: string;
   private map: any;
+  baseUrl = 'http://router.project-osrm.org/route/v1/car/';
+  options = {
+    alternatives: 'true',
+    geometries: 'geojson',
+  };
+  bounds: L.LatLngBounds | null = null;
   estimatedDistance: number = 0;
   estimatedTime: number = 0;
   chosenRouteCoordinates: L.LatLng[] = [];
-  destinations: L.Routing.Waypoint[] = [];
-  destObj: Destination[] = []
-  routing: L.Routing.Control | null = null;
+  destinations: { latitude: number; longitude: number }[] = [];
+  destObj: Destination[] = [];
+  layerPolylines: L.LayerGroup | null = null;
+  layerMarkers: L.LayerGroup | null = null;
+  routes: [
+    {
+      distance: number;
+      duration: number;
+      geometry: { coordinates: [number, number][] };
+    },
+    {
+      distance: number;
+      duration: number;
+      geometry: { coordinates: [number, number][] };
+    }?
+  ][] = [];
   openErrorToast = false;
-  error:boolean = false;
+  error: boolean = false;
   private initMap(): void {
     lrm;
     this.map = L.map('map', {
       center: [45.2671, 19.8335],
-      zoom: 8
+      zoom: 8,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-    });
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+      }
+    );
 
     tiles.addTo(this.map);
+    this.layerPolylines = L.layerGroup().addTo(this.map);
+    this.layerMarkers = L.layerGroup().addTo(this.map);
   }
 
-  constructor(private service: RouteService, private store: Store<StoreType>) { 
-    this.store.select('destinations').subscribe(
-      resData => {
-        this.destObj = resData.destinations;
-      }
-    )  
+  constructor(private service: RouteService, private store: Store<StoreType>) {
+    this.store.select('destinations').subscribe((resData) => {
+      this.destObj = resData.destinations;
+    });
   }
 
   ngAfterViewInit(): void {
-    this.service.trigger$().subscribe(x => this.showDestinations())
+    this.service.trigger$().subscribe((x) => this.showDestinations());
     this.initMap();
   }
 
   showDestinations() {
-    this.destinations = []
+    this.clearMap(true);
+    this.destinations = [];
+    this.routes = [];
     this.error = false;
-    if (this.destObj.filter(elem => elem.address !== '').length === this.destObj.length) {
-      let destPromises = this.destObj.map(async elem=> await this.getLatLong(elem.address))
-      Promise.all(destPromises).then(promises => promises.map(promise => {
-        this.destinations.push(promise)
-        if (this.destinations.length === promises.length)
-          this.addRouting();
-      }));
+    if (
+      this.destObj.filter((elem) => elem.address !== '').length ===
+      this.destObj.length
+    ) {
+      let destPromises = this.destObj.map(async (elem, index, array) => {
+        let outermost = index === 0 || index === array.length - 1;
+        return await this.getLatLong(outermost, elem.address);
+      });
+      Promise.all(destPromises).then((promises) =>
+        promises.map((promise) => {
+          this.destinations.push(promise);
+          if (this.destinations.length === promises.length) this.createRoutes();
+        })
+      );
     }
   }
 
-  addRouting() {
-    if (this.routing) {
-      this.map.removeControl(this.routing);
-    }
+  async createRoutes() {
     if (this.error) {
-      this.openErrorToast = true;
-      this.estimatedDistance = 0;
-      this.estimatedTime = 0;
-      this.setDistanceSlice();
+      this.toggleErrorToast();
       return;
     }
-    this.routing = L.Routing.control({
-      plan: new L.Routing.Plan(this.destinations, {addWaypoints: false, draggableWaypoints: false, createMarker: this.createMarker}),
-      autoRoute: true,
-      showAlternatives: true,
-      lineOptions: { styles: [{ color: 'black', opacity: 0.15, weight: 13 }, { color: 'white', opacity: 0.8, weight: 10 }, { color: '#0A3F68', opacity: 1, weight: 6 }], extendToWaypoints: true, missingRouteTolerance: 0, addWaypoints: false },
-      altLineOptions: { styles: [{ color: 'black', opacity: 0.15, weight: 9 }, { color: 'white', opacity: 0.8, weight: 6 }, { color: '#E1A901', opacity: 1, weight: 4 }], extendToWaypoints: true, missingRouteTolerance: 0 },
-      show: false
-    })
-    .on('routeselected', (e) => {
-      var route = e.route;
-      this.chosenRouteCoordinates = route.coordinates;
-      this.estimatedDistance = route.summary.totalDistance;
-      this.estimatedTime = route.summary.totalTime;
-      this.setDistanceSlice();
-    })
-
-    this.routing.addTo(this.map);
+    for (let i = 0; i < this.destinations.length - 1; i++) {
+      await this.osrm(this.destinations[i], this.destinations[i + 1]);
+    }
+    this.drawPolylines();
+    this.calculateRouteInfo();
   }
 
-  setDistanceSlice() {
-    this.store.dispatch(new DestinationsAction(DestinationsActionType.ADD_ROUTE_DISTANCE, this.estimatedDistance));
-    this.store.dispatch(new DestinationsAction(DestinationsActionType.ADD_ROUTE_TIME, this.estimatedTime));
+  async osrm(
+    start: { latitude: number; longitude: number },
+    end: { latitude: number; longitude: number }
+  ) {
+    const url = `${this.baseUrl}${start.longitude},${start.latitude};${
+      end.longitude
+    },${end.latitude}?${Object.keys(this.options)
+      .map((key) => `${key}=${this.options[key as keyof typeof this.options]}`)
+      .join('&')}`;
+
+    await axios.get(url).then((response) => {
+      const routes = response.data['routes'];
+
+      const mainRoute: {
+        distance: number;
+        duration: number;
+        geometry: { coordinates: [number, number][] };
+      } = routes[0];
+      const alternativeRoute: {
+        distance: number;
+        duration: number;
+        geometry: { coordinates: [number, number][] };
+      } = routes[1];
+
+      alternativeRoute
+        ? this.routes.push([mainRoute, alternativeRoute])
+        : this.routes.push([mainRoute]);
+    });
   }
-  async getLatLong(address: string): Promise<L.Routing.Waypoint> {
+
+  drawPolylines() {
+    this.estimatedDistance = 0;
+    this.estimatedTime = 0;
+    for (const polyline of this.routes) {
+      let mainRoute: { geometry: { coordinates: [number, number][] } } =
+        polyline[0];
+      let mainCoordinates = this.getCoordinates(mainRoute);
+      this.drawMainPolyline(mainCoordinates);
+
+      if (polyline[1]) {
+        let alternativeRoute: {
+          geometry: { coordinates: [number, number][] };
+        } = polyline[1];
+        let alternativeCoordinates = this.getCoordinates(alternativeRoute);
+        this.drawAlternativePolyline(alternativeCoordinates, polyline);
+      }
+    }
+    this.map.fitBounds(this.bounds);
+  }
+
+  getCoordinates(route: { geometry: { coordinates: [number, number][] } }) {
+    const routeCoordinates = route.geometry.coordinates;
+    const routeCoordinatesSwitched = routeCoordinates.map(
+      (coordinate: [number, number]) => [coordinate[1], coordinate[0]]
+    );
+    return routeCoordinatesSwitched;
+  }
+
+  drawMainPolyline(coordinates: any) {
+    const mainRoutePolyline = L.polyline(coordinates, {
+      color: '#E1A901',
+      weight: 4,
+    });
+    this.bounds = mainRoutePolyline.getBounds();
+    mainRoutePolyline.addTo(this.layerPolylines!);
+  }
+  drawAlternativePolyline(
+    coordinates: any,
+    routes: [
+      { geometry: { coordinates: [number, number][] } },
+      { geometry: { coordinates: [number, number][] } }?
+    ]
+  ) {
+    const alternativeRoutePolyline = L.polyline(coordinates, {
+      color: '#0D4F83',
+      weight: 2,
+    });
+
+    alternativeRoutePolyline.addEventListener('click', (event) => {
+      this.clearMap(false);
+      this.routes = this.routes.map((elem) => {
+        if (elem[0].geometry.coordinates === routes[0].geometry.coordinates) {
+          return [elem[1]!, elem[0]];
+        } else return [elem[0], elem[1]!];
+      });
+      this.drawPolylines();
+      this.calculateRouteInfo();
+    });
+    alternativeRoutePolyline.addTo(this.layerPolylines!);
+    this.bounds!.extend(alternativeRoutePolyline.getBounds());
+  }
+
+  calculateRouteInfo() {
+    for (const polyline of this.routes) {
+      let mainRoute = polyline[0];
+      this.estimatedDistance += mainRoute.distance;
+      this.estimatedTime += mainRoute.duration;
+    }
+    this.setRouteInfoSlice();
+  }
+  clearMap(clearMarkers: boolean) {
+    if (!this.layerPolylines) {
+      return;
+    }
+    this.layerPolylines!.clearLayers();
+    if (clearMarkers && this.layerMarkers) this.layerMarkers!.clearLayers();
+  }
+
+  setRouteInfoSlice() {
+    this.store.dispatch(
+      new DestinationsAction(
+        DestinationsActionType.ADD_ROUTE_DISTANCE,
+        this.estimatedDistance
+      )
+    );
+    this.store.dispatch(
+      new DestinationsAction(
+        DestinationsActionType.ADD_ROUTE_TIME,
+        this.estimatedTime
+      )
+    );
+  }
+  async getLatLong(
+    outermost: boolean,
+    address: string
+  ): Promise<{ latitude: number; longitude: number }> {
     let params = `q=${address}&format=json&addressdetails=1&polygon_geojson=0`;
     let longitude: number = 45.2671;
     let latitude: number = 19.8335;
-    let name:string = '';
     const queryStr = new URLSearchParams(params).toString();
-    let result = await axios.get("https://nominatim.openstreetmap.org/search?" + queryStr, { headers: { 'Access-Control-Allow-Origin': '*' } })
+    let result = await axios.get(
+      'https://nominatim.openstreetmap.org/search?' + queryStr,
+      { headers: { 'Access-Control-Allow-Origin': '*' } }
+    );
     if (result.data.length === 0) {
       this.error = true;
     } else {
       longitude = result.data[0].lon;
       latitude = result.data[0].lat;
-      name = result.data[0].display_name;
+      this.createMarker(outermost, new L.LatLng(latitude, longitude), address);
     }
-    return new L.Routing.Waypoint(new L.LatLng(latitude, longitude), name, {allowUTurn: true}); ;
+    return { latitude, longitude };
   }
 
-  createMarker(i: number, wp:L.Routing.Waypoint, nWps: number): L.Marker {
-    if (i === 0 || i === nWps - 1) {
-        return L.marker(wp.latLng, {icon: L.icon({iconUrl:"http://localhost:4200/assets/icons/pin-fill.png", iconSize:[32,32]}), title: wp.name});
+  createMarker(outermost: boolean, latLng: L.LatLng, name: string) {
+    if (outermost) {
+      L.marker(latLng, {
+        icon: L.icon({
+          iconUrl: 'http://localhost:4200/assets/icons/pin-fill.png',
+          iconSize: [32, 32],
+        }),
+        title: name,
+      }).addTo(this.layerMarkers!);
     } else {
-        return L.marker(wp.latLng, {icon: L.icon({iconUrl:"http://localhost:4200/assets/icons/pin-fill-black.png", iconSize:[23,23]}), title: wp.name });
+      L.marker(latLng, {
+        icon: L.icon({
+          iconUrl: 'http://localhost:4200/assets/icons/pin-fill-black.png',
+          iconSize: [23, 23],
+        }),
+        title: name,
+      }).addTo(this.layerMarkers!);
     }
   }
 
   toggleErrorToast = () => {
-    this.openErrorToast =!this.openErrorToast;
-  }
+    this.openErrorToast = !this.openErrorToast;
+  };
 }
