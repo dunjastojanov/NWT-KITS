427be5c8f661a9ac5aaed015ed26d3bbc5e1e08f
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2023-01-03 22:27:30 +0100

    Add drag to replace destinations

--- rocket_front/rocket/src/app/shared/store/destinations-slice/destinations.actions.ts
+++ rocket_front/rocket/src/app/shared/store/destinations-slice/destinations.actions.ts
@@ -1,15 +1,20 @@
-import { Action } from "@ngrx/store";
+import { Action } from '@ngrx/store';
 
 export enum DestinationsActionType {
-    ADD = "ADD",
-    REMOVE = "REMOVE",
-    UPDATE = "UPDATE",
-    ADD_ROUTE_TIME = "ADD_ROUTE_TIME",
-    ADD_ROUTE_DISTANCE = "ADD_ROUTE_DISTANCE"
+  ADD = 'ADD',
+  REMOVE = 'REMOVE',
+  UPDATE = 'UPDATE',
+  SWITCH = 'SWITCH',
+  ADD_ROUTE_TIME = 'ADD_ROUTE_TIME',
+  ADD_ROUTE_DISTANCE = 'ADD_ROUTE_DISTANCE',
 }
 
-export type UpdatePayloadType = {index: number, address: string}
+export type UpdatePayloadType = { index: number; address: string };
+export type SwitchPayloadType = { firstIndex: number; secondIndex: number };
 
 export class DestinationsAction implements Action {
-    constructor(readonly type: string, public payload: string | number | UpdatePayloadType) { }
+  constructor(
+    readonly type: string,
+    public payload: string | number | UpdatePayloadType | SwitchPayloadType
+  ) {}
 }
--- rocket_front/rocket/src/app/shared/utils/input/input/input.component.ts
+++ rocket_front/rocket/src/app/shared/utils/input/input/input.component.ts
@@ -3,25 +3,26 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
   @Input('text') text: string;
   @Input('type') type: string;
-  @Input('name') name!:string;
-  @Input('value') value:string;
+  @Input('name') name!: string;
+  @Input('value') value: string;
+  @Input('marginBottom') marginBottom: string;
 
-  @Input()  prop!: string;
+  @Input() prop!: string;
   @Output() propChange = new EventEmitter<string>();
 
   constructor() {
-    this.value = ''
-    this.type = "text"
-    this.text = "Choose destination"
-   }
-
-  ngOnInit(): void {
+    this.value = '';
+    this.type = 'text';
+    this.text = 'Choose destination';
+    this.marginBottom = 'mb-6';
   }
+
+  ngOnInit(): void {}
 
   onInputChange(event: any) {
     this.prop = event.target.value;
