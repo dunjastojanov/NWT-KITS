114cf54f542068e3dace20de18cbea45ce2e94cf
Dunja <dunjastojanov2@gmail.com>
2023-01-17 19:33:27 +0100

    Fixed mapper after merge

--- rocket_back/rocket/src/main/java/com/uber/rocket/mapper/RideDetailsMapper.java
+++ rocket_back/rocket/src/main/java/com/uber/rocket/mapper/RideDetailsMapper.java
@@ -1,15 +1,21 @@
 package com.uber.rocket.mapper;
 
 import com.uber.rocket.dto.RideDetailsDto;
+import com.uber.rocket.entity.ride.Review;
 import com.uber.rocket.entity.ride.Ride;
+import com.uber.rocket.service.ReviewService;
 import org.springframework.stereotype.Component;
+
 import java.util.stream.Collectors;
+
 @Component
 public class RideDetailsMapper implements Mapper<Ride, RideDetailsDto> {
     private final ReviewMapper reviewMapper;
+    private final ReviewService reviewService;
 
-    public RideDetailsMapper(ReviewMapper reviewMapper) {
+    public RideDetailsMapper(ReviewMapper reviewMapper, ReviewService reviewService) {
         this.reviewMapper = reviewMapper;
+        this.reviewService = reviewService;
     }
 
     @Override
@@ -18,12 +24,12 @@ public class RideDetailsMapper implements Mapper<Ride, RideDetailsDto> {
         return RideDetailsDto
                 .builder()
                 .driver(ride.getDriver().getFullName())
-                .start(ride.getStart().toString())
-                .end(ride.getEnd().toString())
+                .start(ride.getStart())
+                .end(ride.getEnd())
                 .duration(String.valueOf(ride.getDuration()))
                 .price(String.valueOf(ride.getPrice()))
-                .reviews(ride.getReviews().stream().map(reviewMapper::mapToDto).collect(Collectors.toList()))
-                .rating(ride.getRating())
+                .reviews(reviewService.getReviewByRide(ride).stream().map(reviewMapper::mapToDto).collect(Collectors.toList()))
+                .rating(reviewService.getReviewByRide(ride).stream().map(Review::getRating).reduce((double) 0, Double::sum))
                 .build();
     }
 }
