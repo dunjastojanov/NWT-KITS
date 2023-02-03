22623a52546eb9f29b4bce205df793cf83fecf20
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2023-02-03 11:47:14 +0100

    Add tests for ride controller

--- rocket_back/rocket/src/main/java/com/uber/rocket/repository/DriverReportRepository.java
+++ rocket_back/rocket/src/main/java/com/uber/rocket/repository/DriverReportRepository.java
@@ -4,6 +4,9 @@ import com.uber.rocket.entity.user.DriverReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DriverReportRepository extends JpaRepository<DriverReport, Long> {
+    List<DriverReport> findByDriverId(Long driverId);
 }
